fac0ec5b21587646e4c6a33ed0381e1bd06223e3
vikulek <turbina.vika@list.ru>
2025-03-13 20:04:17 +0300

    7

--- Lesson1/ButtonClicker/src/main/java/com/mirea/buttonclicker/MainActivity.java
+++ Lesson1/ButtonClicker/src/main/java/com/mirea/buttonclicker/MainActivity.java
@@ -1,6 +1,10 @@
 package com.mirea.buttonclicker;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvOut;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        EdgeToEdge.enable(this);
+        tvOut = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void OnMyButtonClick(View View)
+    {
+        Toast.makeText(this, "Мой номер по списку № 17", Toast.LENGTH_SHORT).show();
+    }
+    public void OnMyButtonClick1(View View)
+    {
+        Toast.makeText(this, "Это не я сделал", Toast.LENGTH_SHORT).show();
     }
 }
