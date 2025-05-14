4b80f7f5db58005fd0b393e67ec2c52f7d59b93b
vikulek <turbina.vika@list.ru>
2025-05-14 17:22:44 +0300

    8

---
+++ Lesson3/systemintentsapp/src/main/java/com/mirea/turbinava/systemintentsapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.mirea.turbinava.systemintentsapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_call).setOnClickListener(this::onClickCall);
+        findViewById(R.id.button_browser).setOnClickListener(this::onClickOpenBrowser);
+        findViewById(R.id.button_maps).setOnClickListener(this::onClickOpenMaps);
+    }
+
+    public void onClickCall(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:89811112233"));
+        startActivity(intent);
+    }
+
+    public void onClickOpenBrowser(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://developer.android.com"));
+        startActivity(intent);
+    }
+
+    public void onClickOpenMaps(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:55.749479,37.613944"));
+        startActivity(intent);
+    }
+}
