c50c05fffb03079fdfa0fd6491e927a65383b7dd
vikulek <turbina.vika@list.ru>
2025-05-16 16:51:07 +0300

    mirea

---
+++ MireaProject/app/src/main/java/com/mirea/turbinava/mireaproject/DataFragment.java
@@ -0,0 +1,23 @@
+package com.mirea.turbinava.mireaproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.mirea.turbinava.mireaproject.R;
+
+public class DataFragment extends Fragment {
+
+    public DataFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_data, container, false);
+    }
+}
