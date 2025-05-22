055103bbaf0797234d33d8d800fb1a4c40fef1eb
vikulek <turbina.vika@list.ru>
2025-05-23 01:48:40 +0300

    4

--- Lesson6/employeebd/src/main/java/com/mirea/turbinava/employeebd/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.mirea.turbinava.employeebd;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Superhero.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract SuperheroDao superheroDao();
-}
