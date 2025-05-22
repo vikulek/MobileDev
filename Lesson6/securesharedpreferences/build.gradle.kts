7bf953b9c3c839649631cd4cbd88e8a97ef8e23f
vikulek <turbina.vika@list.ru>
2025-05-23 00:23:12 +0300

    2.2

--- Lesson6/securesharedpreferences/build.gradle.kts
+++ Lesson6/securesharedpreferences/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("androidx.security:security-crypto:1.1.0-alpha06")
 }
--- Lesson6/securesharedpreferences/src/main/java/com/mirea/turbinava/securesharedpreferences/MainActivity.java
+++ Lesson6/securesharedpreferences/src/main/java/com/mirea/turbinava/securesharedpreferences/MainActivity.java
@@ -1,24 +1,46 @@
 package com.mirea.turbinava.securesharedpreferences;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKey;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView poetName = findViewById(R.id.poetName);
+        ImageView poetImage = findViewById(R.id.poetImage);
+
+        try {
+            MasterKey masterKey = new MasterKey.Builder(this)
+                    .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                    .build();
+
+            SharedPreferences encryptedPrefs = EncryptedSharedPreferences.create(
+                    this,
+                    "secure_prefs",
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+
+            if (!encryptedPrefs.contains("poet")) {
+                encryptedPrefs.edit().putString("poet", "Сергей Александрович Есенин").apply();
+            }
+
+            String poet = encryptedPrefs.getString("poet", "Неизвестно");
+            poetName.setText(poet);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
